d0afc5a04d9159408923c904e5e27a9717556c56
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2025-05-17 18:47:42 +0300

    Fixed burger menu

--- src/main.js
+++ src/main.js
@@ -3,14 +3,29 @@
         const burgerMenu = document.querySelector('.icon-burger-menu');
         const closeMenu = document.querySelector('.icon-close-burger-menu');
         const menu = document.querySelector('.mobile-menu');
+        const header = document.querySelector('.header');
+        const headerButton = document.querySelector('.header-button');
+        const navigationLinks = document.querySelectorAll('.navigation-link');
+
+        navigationLinks.forEach((link) => {
+            link.addEventListener('click', onToggleMenu);
+        });
 
         function onToggleMenu() {
-            burgerMenu.classList.toggle('is-open');
-            menu.classList.toggle('is-open');
-            closeMenu.classList.toggle('is-open');
-            document.body.classList.toggle('body-no-scroll');
+            burgerMenu.classList.toggle('is-menu-open');
+            menu.classList.toggle('is-menu-open');
+            closeMenu.classList.toggle('is-menu-open');
+            header.classList.toggle('is-menu-open');
         }
 
+        function onCloseMenu() {
+            burgerMenu.classList.remove('is-menu-open');
+            menu.classList.remove('is-menu-open');
+            closeMenu.classList.remove('is-menu-open');
+            header.classList.remove('is-menu-open');
+        }
+
+        headerButton.addEventListener('click', onCloseMenu);
         burgerMenu.addEventListener('click', onToggleMenu);
         closeMenu.addEventListener('click', onToggleMenu);
     });
