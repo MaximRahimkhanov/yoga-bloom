addb78be9c0e485a1041d0873c11012582054935
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2025-05-20 22:41:56 +0300

    Made header fixed.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
         const burgerMenu = document.querySelector('.icon-burger-menu');
         const closeMenu = document.querySelector('.icon-close-burger-menu');
         const menu = document.querySelector('.mobile-menu');
-        const header = document.querySelector('.header');
         const headerButton = document.querySelector('.header-button');
         const navigationLinks = document.querySelectorAll('.navigation-link');
 
@@ -11,14 +10,12 @@
             burgerMenu.classList.toggle('is-menu-open');
             menu.classList.toggle('is-menu-open');
             closeMenu.classList.toggle('is-menu-open');
-            header.classList.toggle('is-menu-open');
         }
 
         function onCloseMenu() {
             burgerMenu.classList.remove('is-menu-open');
             menu.classList.remove('is-menu-open');
             closeMenu.classList.remove('is-menu-open');
-            header.classList.remove('is-menu-open');
         }
 
         navigationLinks.forEach((link) => {
