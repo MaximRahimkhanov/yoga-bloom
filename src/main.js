0e8fd477962ed089948e73f8a32dc81eaa29672c
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2025-05-21 14:42:31 +0300

    Added shadow to header

--- src/main.js
+++ src/main.js
@@ -5,17 +5,20 @@
         const menu = document.querySelector('.mobile-menu');
         const headerButton = document.querySelector('.header-button');
         const navigationLinks = document.querySelectorAll('.navigation-link');
+        const header = document.querySelector('.header');
 
         function onToggleMenu() {
             burgerMenu.classList.toggle('is-menu-open');
             menu.classList.toggle('is-menu-open');
             closeMenu.classList.toggle('is-menu-open');
+            header.classList.toggle('is-menu-open');
         }
 
         function onCloseMenu() {
             burgerMenu.classList.remove('is-menu-open');
             menu.classList.remove('is-menu-open');
             closeMenu.classList.remove('is-menu-open');
+            header.classList.remove('is-menu-open');
         }
 
         navigationLinks.forEach((link) => {
